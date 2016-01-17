3dac5d0cfb97956472d0b6d9b173000105159f6d
Simon Legner <Simon.Legner@gmail.com>
2016-01-17 10:20:04 +0100

    Add proxy support
    The configuration is read from `network.cfg` in the working directory
    and has the same structure as https://github.com/onyxbits/Raccoon/

--- src/main/java/de/onyxbits/dummydroid/CheckinWorker.java
+++ src/main/java/de/onyxbits/dummydroid/CheckinWorker.java
@@ -2,7 +2,10 @@ package de.onyxbits.dummydroid;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.Properties;
 
 import javax.swing.SwingWorker;
 
@@ -12,6 +15,12 @@ import com.akdeniz.googleplaycrawler.GooglePlay.ResponseWrapper;
 import com.akdeniz.googleplaycrawler.GooglePlay.UploadDeviceConfigRequest;
 import com.akdeniz.googleplaycrawler.GooglePlayAPI;
 import com.akdeniz.googleplaycrawler.Utils;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
  * A swingworker for uploading the checkinrequest to Play and getting a GSF ID.
@@ -24,14 +33,45 @@ class CheckinWorker extends SwingWorker<String, Object> {
 	private FormData formData;
 	private CheckinForm callback;
 
+	/**
+	 * Name of the file containing the network config
+	 */
+	public static final String NETCFG = "network.cfg";
+	public static final String PROXYHOST = "proxyhost";
+	public static final String PROXYPORT = "proxyport";
+	public static final String PROXYUSER = "proxyuser";
+	public static final String PROXYPASS = "proxypass";
+
 	public CheckinWorker(CheckinForm callback, FormData formData) {
 		this.formData = formData;
 		this.callback = callback;
 	}
 
+	protected void setProxy(HttpClient client) throws IOException {
+		File cfgfile = new File(NETCFG);
+		if (cfgfile.exists()) {
+			Properties cfg = new Properties();
+			cfg.load(new FileInputStream(cfgfile));
+			String ph = cfg.getProperty(PROXYHOST, null);
+			String pp = cfg.getProperty(PROXYPORT, null);
+			String pu = cfg.getProperty(PROXYUSER, null);
+			String pw = cfg.getProperty(PROXYPASS, null);
+			if (ph == null || pp == null) {
+				return;
+			}
+			final HttpHost proxy = new HttpHost(ph, Integer.parseInt(pp));
+			client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+			if (pu != null && pw != null) {
+				((DefaultHttpClient) client).getCredentialsProvider().setCredentials(
+						new AuthScope(proxy), new UsernamePasswordCredentials(pu, pw));
+			}
+		}
+	}
+
 	@Override
 	protected String doInBackground() throws Exception {
 		GooglePlayAPI api = new GooglePlayAPI(formData.getUsername(), formData.getPassword());
+		setProxy(api.getClient());
 		// this first checkin is for generating android-id
 		AndroidCheckinResponse checkinResponse = api.postCheckin(Utils.generateAndroidCheckinRequest()
 				.toByteArray());
